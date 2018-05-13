18f822785d4327434db5fcb2139d1c2d86ee947d
John Kennedy (codejockie) <devjckennedy@gmail.com>
2018-05-13 11:00:55 +0100

    register a lifecycle hook with the routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,16 +6,19 @@ import { WorkComponent }        from './work/work.component';
 import { AddressComponent }     from './address/address.component';
 import { ResultComponent }      from './result/result.component';
 
+import { WorkflowGuard }        from './workflow/workflow-guard.service';
+import { WorkflowService }      from './workflow/workflow.service';
+
 
 export const appRoutes: Routes = [
     // 1st Route
     { path: 'personal', component: PersonalComponent },
     // 2nd Route
-    { path: 'work', component: WorkComponent },
+    { path: 'work', component: WorkComponent, canActivate: [WorkflowGuard] },
     // 3rd Route
-    { path: 'address', component: AddressComponent },
+    { path: 'address', component: AddressComponent, canActivate: [WorkflowGuard] },
     // 4th Route
-    { path: 'result', component: ResultComponent },
+    { path: 'result', component: ResultComponent, canActivate: [WorkflowGuard] },
     // 5th Route
     { path: '', redirectTo: '/personal', pathMatch: 'full' },
     // 6th Route
@@ -24,7 +27,8 @@ export const appRoutes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes, { useHash: true } )],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [WorkflowGuard]
 })
 
 export class AppRoutingModule {}
