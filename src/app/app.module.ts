97f55bc70e1c1c1a8840154a8416c41f30a0c6e2
John Kennedy (codejockie) <devjckennedy@gmail.com>
2018-05-13 09:44:13 +0100

    create route module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }        from '@angular/forms';
 
-import { AppComponent } from './app.component';
+/* App Root */
+import { AppComponent }       from './app.component';
+import { NavbarComponent }    from './navbar/navbar.component';
+
+/* Feature Components */
+import { PersonalComponent }  from './personal/personal.component';
+import { WorkComponent }      from './work/work.component';
+import { AddressComponent }   from './address/address.component';
+import { ResultComponent }    from './result/result.component';
+
+/* Routing Module */
+import { AppRoutingModule }   from './app-routing.module';
+
+/* Shared Service */
+import { FormDataService }    from './data/formData.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PersonalComponent,
+    WorkComponent,
+    AddressComponent,
+    ResultComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [{ provide: FormDataService, useClass: FormDataService }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
