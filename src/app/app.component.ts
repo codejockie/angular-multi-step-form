bdfef6db82657b6659cacb428c38ff803d80c571
John Kennedy (codejockie) <devjckennedy@gmail.com>
2018-05-13 09:51:44 +0100

    create app component and its view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+
+import { FormDataService } from "./data/formData.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "multi-step-wizard-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
+  title = "Multi-Step Wizard";
+  @Input() formData;
+
+  constructor(private formDataService: FormDataService) {}
+
+  ngOnInit() {
+    this.formData = this.formDataService.getFormData();
+    console.log(this.title + " loaded!");
+  }
 }
